5084bc16bda68abfa7889ced0389b714d94ccdd7
Shiro <shirokitsunelp@gmail.com>
2025-08-08 21:46:30 +0200

    updated trivia answer

--- src/events/interactions/buttons.js
+++ src/events/interactions/buttons.js
@@ -1,4 +1,6 @@
-const { Events, ButtonBuilder, ActionRowBuilder, ButtonStyle, MessageFlags } = require('discord.js')
+const { Events, ButtonBuilder, ActionRowBuilder, MessageFlags, EmbedBuilder } = require('discord.js')
+
+const { color } = require('./../../config/color.json')
 
 module.exports = {
     name: Events.InteractionCreate,
@@ -23,36 +25,46 @@ module.exports = {
             const selectedOptionIndex = parseInt(interaction.customId.split('_')[2])
             const correctAnswerIndex = triviaData.answer
 
-            const originalMessage = interaction.message
-            const updatedComponents = []
-
-            for(const row of originalMessage.components) {
+            const originalComponents = interaction.message.components.map(row => {
                 const newRow = new ActionRowBuilder()
-                for (const button of row.components) {
-                    const newButton = ButtonBuilder.from(button)
-
-                    newButton.setDisabled(true)
-
-                    const buttonIndex = parseInt(newButton.data.custom_id.split('_')[2])
-
-                    if(buttonIndex === correctAnswerIndex) {
-                        newButton.setStyle(ButtonStyle.Success)
-                    } else if(buttonIndex === selectedOptionIndex) {
-                        newButton.setStyle(ButtonStyle.Danger)
-                    }
+                row.components.forEach(button => {
+                    const newButton = ButtonBuilder.from(button).setDisabled(true)
                     newRow.addComponents(newButton)
-                }
-                updatedComponents.push(newRow)
-            }
+                })
+                return newRow
+            })
+            await interaction.message.edit({ components: originalComponents })
 
-            await originalMessage.edit({ components: updatedComponents })
-
-            if(selectedOptionIndex === correctAnswerIndex) {
-                await interaction.followUp({ content: '**Correct!** Well done!', flags: MessageFlags.Ephemeral })
+            let embed
+            if (selectedOptionIndex === correctAnswerIndex) {
+                embed = new EmbedBuilder()
+                    .setTitle('Correct!')
+                    .setColor(color.success)
+                    .setAuthor({
+                        name: ayumi.user.username,
+                        iconURL: ayumi.user.displayAvatarURL({ dynamic: true, size: 2048 })
+                    })
+                    .setDescription('You answered the question correctly! Congratulations.')
             } else {
                 const correctOptionText = triviaData.options ? triviaData.options[correctAnswerIndex] : 'Unknown'
-                await interaction.followUp({ content: `**Incorrect.** The correct answer was: **${correctOptionText}**`, flags: MessageFlags.Ephemeral })
+                const selectedOptionText = triviaData.options ? triviaData.options[selectedOptionIndex] : 'Unknown'
+                
+                embed = new EmbedBuilder()
+                    .setTitle('Incorrect!')
+                    .setColor(color.error)
+                    .setAuthor({
+                        name: ayumi.user.username,
+                        iconURL: ayumi.user.displayAvatarURL({ dynamic: true, size: 2048 })
+                    })
+                    .setDescription('Your answer was unfortunately wrong.')
+                    .addFields(
+                        { name: 'Your Answer', value: `\`${selectedOptionText}\``, inline: true },
+                        { name: 'Correct Answer', value: `\`${correctOptionText}\``, inline: true }
+                    )
             }
+
+            await interaction.followUp({ embeds: [embed], flags: MessageFlags.Ephemeral })
+
             delete ayumi.trivia
         }
     },
